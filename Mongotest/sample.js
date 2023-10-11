fa88f542a43a03586cdbfa8827e847eb41c8cc85
Nishanth <nishanthanand2001@gmail.com>
2023-10-11 19:54:06 +0530

    query

--- Mongotest/sample.js
+++ Mongotest/sample.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import User from "./models/User.js";
+import Order from "./models/Order.js";
 dotenv.config();
 
 const getActiveUserName = async () => {
@@ -24,17 +25,71 @@ const getUserByEmail = async (inputEmail) => {
   return user.name;
 };
 
-const getUsersAbove2500 = () => {};
+const getOrdersAbove2500 = async () => {
+  const dbData = await Order.find({
+    price: {
+      $gte: 2500,
+    },
+  });
+  const returnData = [];
+  for (let i = 0; i < dbData.length; i++) {
+    const data = dbData[i];
+    returnData.push(data.price);
+  }
+  return returnData;
+};
+
+const getStatusOfOrders = async () => {
+  const status = await Order.count({
+    status: { $in: ["cancelled"] },
+  });
+  //status =>[{userId,price,status,returnDays}]
+  const returnData = [];
+  for (let i = 0; i < status.length; i++) {
+    const order = status[i];
+    //order=>[{userId:"",price:"",status}]
+
+    returnData.push({
+      orderId: order.uid,
+      userName: order.name,
+      userId: order.userId,
+    });
+  }
+  return returnData;
+};
+
+const getRolesOfUsers = async () => {
+  const roles = await User.find({
+    roles: { $in: ["admin", "manager", "employee"] },
+  });
+};
+
+const getRoleCount = async () => {
+  const users = await User.find({}).lean();
+  const roleCount = {};
+  users.forEach((user) => {
+    // for (let i = 0; i < users.length; i++) {
+    //   const user = users[i];
+    // }
+
+    const userRole = user.role;
+    if (roleCount.hasOwnProperty(userRole)) {
+      roleCount[userRole] = roleCount[userRole] + 1;
+    } else {
+      roleCount[userRole] = 1;
+    }
+  });
+  return roleCount;
+};
 
 const main = async () => {
   let conn;
-  const inputEmail = "Justen.Kirlin@gmail.com";
+
   try {
     conn = await mongoose.connect(process.env.MONGODB_URL);
 
-    const userName = await getUserByEmail(inputEmail);
-
-    console.log(userName);
+    const roleByCount = await getRoleCount();
+    console.log(roleByCount);
   } catch (err) {
     console.error(err);
   } finally {
